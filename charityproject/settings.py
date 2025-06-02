1ae9b0357f0619ede27afdb6c2a848a794b5e24d
Muhammad Moosa <computerknowledge817@gmail.com>
2025-06-02 19:44:06 +0500

    updated requirements , archived notice added

--- charityproject/settings.py
+++ charityproject/settings.py
@@ -155,6 +155,8 @@ AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_S3_CUSTOM_DOMAIN = CLOUDFRONT_DOMAIN
+MEDIA_URL = f"{CLOUDFRONT_DOMAIN}/"
+
 
 if DEBUG == False:
     STATIC_LOCATION = "static"
@@ -163,3 +165,6 @@ if DEBUG == False:
     STATICFILES_STORAGE = "charityproject.storage_backends.StaticStorage"
 else:
     STATIC_URL = "static/"
+
+
+print(STATIC_URL)
